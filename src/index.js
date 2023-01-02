d7da7bbcd1c0ac51ab6a7d711563c25e8ed20ebf
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-01-02 21:01:32 +0300

    init: add solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const str = Math.abs(n).toString();
+    let i = 1;
+    let result = '';
+    while (i <= str.length) {
+      result = result + str[str.length-i];
+      i++;
+    }
+    return result;
 }
